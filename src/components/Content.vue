93c11a5544283360030045b7cd2c2976297691f7
mouselu00 <mouselu00@gmail.com>
2017-07-29 21:58:55 +0800

    Create component ContentDetial

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="content">
+    
     <v-layout row>
       <v-flex>
         <v-card>
@@ -53,38 +54,22 @@
       </v-flex>
     </v-layout>
   
-    <v-layout row wrap align-end class="mt-2">
-      <v-flex xs4 v-for="item in items" :key="item.id">
-        <v-card class="mb-2">
-          <v-card-media class="white--text" height="200px" :src="item.img">
-            <v-container fill-height fluid>
-              <v-layout fill-height>
-                <v-flex xs12 align-end flexbox>
-                  <span class="headline">{{ item.title }}</span>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-media>
-          <v-card-title>
-            <div>
-              <span class="grey--text">$ {{ item.price }}</span>
-              <br>
-              <span>{{ item.desc }}</span>
-            </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn flat class="orange--text" @click.native="add(item.id)" :disabled="dis(item.id)">BUY</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-  
-    </v-layout>
+    <ContentDetial :items='items' :carts='carts' @addToCart='addToCart'></ContentDetial>
+
   </div>
 </template>
 
 <script>
+
+import ContentDetial from './ContentDetial.vue'
+
 export default {
   name: 'content',
+
+  components: {
+    ContentDetial
+  },
+
   data() {
     return {
       dialog: false,
@@ -100,18 +85,13 @@ export default {
     })
   },
   methods: {
-    add(index) {
-
+    addToCart(index) {
       const cart = this.items.find((data) => {
         return data.id == index
       })
       this.carts.push(cart)
     },
-    dis(index) {
 
-      const bool = this.carts.find((data) => data.id === index)
-      return bool ? true : false
-    },
     remove(index) {
       this.carts.splice(index, 1)
     }
---
+++ src/components/ContentDetial.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-layout row wrap align-end class="mt-2">
+      <v-flex xs4 v-for="item in items" :key="item.id">
+        <v-card class="mb-2">
+          <v-card-media class="white--text" height="200px" :src="item.img">
+            <v-container fill-height fluid>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline">{{ item.title }}</span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-media>
+          <v-card-title>
+            <div>
+              <span class="grey--text">$ {{ item.price }}</span>
+              <br>
+              <span>{{ item.desc }}</span>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+              <v-btn flat class="orange--text" @click.native="add(item.id)" :disabled="dis(item.id)">BUY</v-btn>  
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+  
+    </v-layout>
+</template>
+<script>
+  export default{
+    name: 'ContentDetial',
+
+    props: ['items' , 'carts'],
+
+    data(){
+      return {
+        disabled: false,
+      }
+    },
+
+    methods: {
+      add(id){
+        this.$emit('addToCart' , id)
+        this.disabled = true
+      },
+      dis(index) {
+        const bool = this.carts.find((data) => data.id === index)
+        return bool ? true : false
+      },
+    }
+
+  }
+</script>
